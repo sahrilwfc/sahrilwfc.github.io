0046959a214616d2ded75cd24a545e9c8e0ae952
developer <developer@polije.ac.id>
2026-02-10 21:54:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+const typed = new Typed('.text-animate', {
+    strings: ['Frontend Developer', 'Web Designer', 'YouTuber'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
